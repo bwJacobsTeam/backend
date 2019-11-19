5f8c7ba2202b89f7d762337e8d1215d68fd5b767
theJOHNwatt <j.watt10@gmail.com>
2019-11-19 10:06:36 -0800

    testing enpoint for deploying

--- users/users-router.js
+++ users/users-router.js
@@ -1,9 +1,28 @@
 const router = require("express").Router();
-
+const restricted = require('../auth/restricted-middleware.js');
 const Users = require('./users-model.js');
 
-router.get('/', (req, res) => {
-
+router.get('/campaigns', restricted, (req, res) => {
+    Users.findCampaign()
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        });
 })
+
+
+router.get('/:id/donations',  (req, res) => {
+    Users.findDonationsByUser(req.params.id)
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        });
+})
+
+
 
 module.exports = router;
